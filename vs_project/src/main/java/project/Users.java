3dfa2ae3b77af60f4f90b1acc5ba5143a518a1b8
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2026-05-06 11:39:36 +0200

    klar med Users klassen

---
+++ vs_project/src/main/java/project/Users.java
@@ -0,0 +1,33 @@
+package project;
+
+public class Users {
+    private String id;
+    private String name;
+    private String email;
+
+    public Users(String id, String name, String email){
+        this.id=id;
+        this.name=name;
+        this.email=email;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
