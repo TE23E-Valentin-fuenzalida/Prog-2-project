dc3189ec0166d45b9dc08105ced53997a5dbf7fc
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2026-05-08 14:50:15 +0200

    klar med SuspendedUsersList get_allSuspendedUsers

--- vs_project/src/main/java/project/SuspendedUsersList.java
+++ vs_project/src/main/java/project/SuspendedUsersList.java
@@ -1,5 +1,54 @@
 package project;
 
+//GSON objekt som vi behöver
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+//importera Type för att hjälpa json att omvandla data
+import java.lang.reflect.Type;
+//Unirest objekt som vi behöver
+import kong.unirest.Unirest;
+import kong.unirest.HttpResponse;
+import kong.unirest.UnirestConfigException;
+import kong.unirest.UnirestException;
+
+//importera Fill hanteringen
+import java.nio.file.*;
+import java.time.chrono.HijrahChronology;
+import java.io.EOFException;
+import java.io.IOException;
+// arraylist för att lagra objekt
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import javax.swing.plaf.FontUIResource;
+
 public class SuspendedUsersList {
-    private Arraylist<SuspendedUsers> 
+    private ArrayList<SuspendedUsers> listSuspendedUsers = new ArrayList<>();
+    Gson gson = new Gson();
+
+    public void get_allSuspendedUsers() {
+
+        HttpResponse<String> get_all_ResponseSuspendedUsers;
+        // hämtar URL och lagrar den i servern
+        try {
+            get_all_ResponseSuspendedUsers = Unirest.get(Main.baseURL + "suspended").asString();
+        } catch (Exception e) {
+            IO.println("Undantag uppkoppling " + e.getLocalizedMessage());
+            return;
+        }
+        // kontrollera om servern svarade med "200 OK"
+        int status = get_all_ResponseSuspendedUsers.getStatus();
+        if (status != 200) {
+            IO.println("Fel från servern, statuskod: " + status);
+            return;
+        }
+        // hämtar json texten
+        String get_all_bodySuspendedUsers = get_all_ResponseSuspendedUsers.getBody();
+        // omvandla json-text till i ArrayList med bok-objekt
+        Type postListType1 = new TypeToken<ArrayList<Users>>() {
+        }.getType();
+        listSuspendedUsers = gson.fromJson(get_all_bodySuspendedUsers, postListType1);
+    }
 }
