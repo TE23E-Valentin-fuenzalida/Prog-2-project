27195e2ef17be8d9a8679a7588630d71e6450b9c
Valentin fuenzalida <valentin.fuenzalidaribbing@elev.ga.ntig.se>
2026-05-10 21:05:24 +0200

    Fixat så det finns en huvudMeny och flera underMenyer inne i biblotekssytem klassen

--- vs_project/src/main/java/project/Biblotekssystem.java
+++ vs_project/src/main/java/project/Biblotekssystem.java
@@ -6,21 +6,102 @@ public class Biblotekssystem {
     private static BooksList Booksregister;
     private static MagazinesList Magazinesregister;
 
-    public String meny() {
-        IO.println(
-        """
-        välj vad du vill göra
-            1. Hämta alla böcker, tidning, kunder eller avstängda
-            2. Hämta en bok, tidning, kund eller avstängd
-            3. Skapa ny bok, tidning, kunder eller avstängd
-            4. Hitta kund
-            5. Hitta bok eller tidning
-            6. Ta bort bok  eller tidning
-            7. Ta bort kund eller avstängd
-            8. Skriva ut sorterat böcker, tidningar eller kunder
-            9. Vilka användare får inte låna
-            10. Avsluta
-        """);
+    // huvudmeny
+    public String huvudMeny() {
+        IO.println("""
+                Välj vad du vill göra
+                    1. Hämta ut (alla)
+                    2. Hämta ut (en)
+                    3. Skapa nytt
+                    4. Söka
+                    5. Ta bort
+                    6. Skriva ut (sorterat)
+                    7. Vilka får låna
+                    8. Avsluta
+                        """);
+        String val = IO.readln();
+
+        return val;
+    }
+
+    public String hämtaUtAllaMeny() {
+
+        IO.println("""
+                    Hämta ut (alla)
+                    1. Böcker
+                    2. tidningar
+                    3. kunder
+                    4. avstängda
+                """);
+        String val = IO.readln();
+
+        return val;
+    }
+
+    public String hämtaUtEnMeny() {
+
+        IO.println("""
+                    Hämta ut (en)
+                    1. bok
+                    2. tidning
+                    3. kund
+                    4. avstängd
+                """);
+        String val = IO.readln();
+
+        return val;
+    }
+
+    public String SkapaNyttMeny() {
+
+        IO.println("""
+                    Skapa nytt
+                    1. Ny bok
+                    2. Ny tidning
+                    3. Ny kund
+                    4. Ny avstängd
+                """);
+        String val = IO.readln();
+
+        return val;
+    }
+
+    public String SökaMeny() {
+
+        IO.println("""
+                    Söka
+                    1. bok
+                    2. tidning
+                    3. kund
+                    4. avstängd
+                """);
+        String val = IO.readln();
+
+        return val;
+    }
+
+    public String TabortMeny() {
+
+        IO.println("""
+                    Ta bort
+                    1. bok
+                    2. tidning
+                    3. kund
+                    4. avstängd
+                """);
+        String val = IO.readln();
+
+        return val;
+    }
+
+    public String SkriaUtMeny() {
+
+        IO.println("""
+                    Skriva ut (sorterat)
+                    1. bok
+                    2. tidning
+                    3. kund
+                """);
         String val = IO.readln();
 
         return val;
